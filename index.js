dcd905d8ec8e3ec4da6db7330e6990b074483e1f
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-05-17 11:39:39 +0000

    index.js

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ console.log('hello, world');
 console.table([
     {first: 'rene', last: 'magrite',},
 {first: 'chaim', last: 'soutine',},
-{first: 'henri', last: 'matisse', birthday: '04111988',}]);
+{first: 'henri', last: 'matisse', birthday: '04111988',},
+{first: 'muhammad', last: 'mai kudi', birthday: '15251996',}]);
+
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
+
+const myArray = ["I", "love", "chocolate", "frogs",];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+// the join() function takes an array, joins
+// all the array items together into a single
+// string, and returns this new string
+
+
+
