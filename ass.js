b0bf0a855f3b19d9a5fd9b91dab36b1cc1d3d9e5
Engr Muazzam Kimba <ibrahimkimba75@gmail.com>
2023-06-08 13:39:14 +0000

    ass.js

--- ass.js
+++ ass.js
@@ -1,1 +1,6 @@
 let answer = parseInt(prompt('PLease enter the number you want to buzzfuzz'));
+for (let i=1; i<=answer; i++){
+    if (i%3===0){
+        console.log('buzz');}
+        else {console.log('i');}
+}
